dc64e3223bd661045dba05fa9ecd0dd44fa9b69f
Lucas Passos <lucas.depassos@yahoo.com.br>
2020-08-25 19:07:58 -0300

    First_commit

---
+++ src/components/Home.vue
@@ -0,0 +1,36 @@
+<template>
+<v-container fluid>
+    <div>
+
+    </div>
+
+</v-container>
+</template>
+
+<script>
+export default {
+
+    data: function () {
+        return {
+            groupedWords: [{
+                    name: 'you',
+                    amount: 900
+                },
+                {
+                    name: 'he',
+                    amount: 853
+                },
+                {
+                    name: 'i',
+                    amount: 1234
+                }
+            ]
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
